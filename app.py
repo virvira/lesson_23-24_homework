35d1b0881c9b66e60aa2bffb742b75f72acc36a0
virvira <v.saenko@skyeng.ru>
2022-12-01 19:04:56 +0500

    added code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+from utils import get_data_from_file, filter_query, map_query, unique_query, sort_query, limit_query
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,10 +9,39 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+@app.route("/perform_query", methods=["POST"])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    req_json = request.json
+    valid_commands = {
+        'filter': filter_query,
+        'map': map_query,
+        'unique': unique_query,
+        'sort': sort_query,
+        'limit': limit_query
+    }
+    try:
+        file_name = req_json['file_name']
+        cmd1 = req_json['cmd1']
+        value1 = req_json['value1']
+        cmd2 = req_json['cmd2']
+        value2 = req_json['value2']
+    except Exception as e:
+        return {"error": f"Поле {e} не найдено"}, 400
+
+    file_path = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(file_path):
+        return {"error": f"Файл {file_name} не найден"}, 400
+
+    data = get_data_from_file(file_path)
+    res = []
+
+    if cmd1 and cmd2 in valid_commands:
+        res_cmd1 = valid_commands[cmd1](value1, data)
+        res = valid_commands[cmd2](value2, res_cmd1)
+
+    if res is not None:
+        return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ utils.py
@@ -0,0 +1,40 @@
+def get_data_from_file(file_name: str) -> list:
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data_list = list(map(lambda row: row.strip(), f))
+    return data_list
+
+
+def filter_query(param: str, data_list: list) -> list:
+    res = filter(lambda row: param in row, data_list)
+    return list(res)
+
+
+def map_query(param: str, data_list: list) -> list:
+    column_number = int(param)
+    res = map(lambda row: row.split(' ')[column_number], data_list)
+    return list(res)
+
+
+def unique_query(data_list: list, *args, **kwargs) -> list:
+    list_of_unique_rows = []
+    unique_rows = set()
+
+    for row in data_list:
+        if row in unique_rows:
+            continue
+        else:
+            list_of_unique_rows.append(row)
+            unique_rows.add(row)
+
+    return list_of_unique_rows
+
+
+def sort_query(param: str, data_list: list) -> list:
+    reverse = False if param == 'asc' else True
+    res = sorted(data_list, reverse=reverse, key=lambda row: row)
+    return list(res)
+
+
+def limit_query(param: str, data_list: list) -> list:
+    rows_count = int(param)
+    return data_list[:rows_count]
