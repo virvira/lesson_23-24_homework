9ace95039b1e9ee2d2ae7207b3aea1e23b9bb23a
virvira <v.saenko@skyeng.ru>
2022-12-11 17:08:46 +0300

    added edits

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+from typing import Union, Dict, Optional, Any, Iterator, List
+
 from utils import get_data_from_file, filter_query, map_query, unique_query, sort_query, limit_query, regex_query
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
@@ -10,9 +12,9 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
 @app.route("/perform_query", methods=["POST"])
-def perform_query():
-    req_json = request.json
-    valid_commands = {
+def perform_query() -> Union[Response, str]:
+    req_json: Union[Dict[str, str], Any] = request.json
+    valid_commands: Dict[str, Any] = {
         'filter': filter_query,
         'map': map_query,
         'unique': unique_query,
@@ -21,20 +23,20 @@ def perform_query():
         'regex': regex_query
     }
     try:
-        file_name = req_json['file_name']
-        cmd1 = req_json['cmd1']
-        value1 = req_json['value1']
-        cmd2 = req_json['cmd2']
-        value2 = req_json['value2']
+        file_name: str = req_json['file_name']
+        cmd1: str = req_json['cmd1']
+        value1: str = req_json['value1']
+        cmd2: str = req_json['cmd2']
+        value2: str = req_json['value2']
     except Exception as e:
-        return {"error": f"Поле {e} не найдено"}, 400
+        return Response(f"Поле {e} не найдено", status=400)
 
-    file_path = os.path.join(DATA_DIR, file_name)
+    file_path: str = os.path.join(DATA_DIR, file_name)
     if not os.path.exists(file_path):
-        return {"error": f"Файл {file_name} не найден"}, 400
+        return Response(f"Файл {file_name} не найден", status=400)
 
-    data = get_data_from_file(file_path)
-    res = []
+    data: Iterator[str] = get_data_from_file(file_path)
+    res: List[str] = []
 
     if cmd1 and cmd2 in valid_commands:
         res_cmd1 = valid_commands[cmd1](param=value1, data_list=data)
